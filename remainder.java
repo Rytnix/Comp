ecb970ba3118a4dc7c2cad6c4581989cb12fe7d9
Rytnix <Rytnix@users.noreply.github.com>
2021-02-01 16:44:26 +0530

    Create remainder.java

---
+++ remainder.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ class remainder 
+ {
+     public static void main(String[]args)
+     {
+         Scanner sc= new Scanner(System.in);
+
+         int t,a,b;
+         t=sc.nextInt();
+         while(t>0)
+         {
+            a=sc.nextInt();
+            b=sc.nextInt();
+            System.out.println((a%b));
+            t--;
+         }
+         sc.close();
+
+     }
+    
+}
